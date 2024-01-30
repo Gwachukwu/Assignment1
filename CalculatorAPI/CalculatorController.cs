e147c791446fc4c64121dbf12d1970ac00bd9e61
gwachukwu <gwachukwuelebeke@gmail.com>
2024-01-29 22:21:06 -0500

    Complete assignment

---
+++ CalculatorAPI/CalculatorController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        public decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public decimal Subtract(decimal a, decimal b)
+        {
+            return a - b;
+        }
+
+        public decimal Multiply(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+        public decimal Divide(decimal a, decimal b)
+        {
+            return a / b;
+        }
+
+        public decimal Modulo(decimal a, decimal b)
+        {
+            return a % b;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Assignment1
+* MAPD's Backend Development first assignment
+
+## Requirements
+* This api works with query strings a and b.
+  a will always be the firt number in order while b is the second
+
+__Enjoy__
